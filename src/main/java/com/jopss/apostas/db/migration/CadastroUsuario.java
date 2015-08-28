861195101ad8f8f6403bf53470550667471d4b1c
Kelsen <kelsen.brito@gmail.com>
2015-08-28 17:43:56 -0300

    migration

--- src/main/java/com/jopss/apostas/db/migration/CadastroUsuario.java
+++ src/main/java/com/jopss/apostas/db/migration/CadastroUsuario.java
@@ -5,6 +5,7 @@ import com.github.mongobee.changeset.ChangeSet;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import java.util.Date;
 
 @ChangeLog(order = "003")
@@ -16,13 +17,15 @@ public class CadastroUsuario {
                 DBCollection usuario = db.getCollection("usuario");
                 DBCollection perfil = db.getCollection("perfil");
                 
+                DBObject ob = perfil.findOne(new BasicDBObject("nome", "Perfil Administrador"), new BasicDBObject("_id", 1));
+                
                 BasicDBObject admin = new BasicDBObject();
                 admin.append("dataatualizacao", null);
                 admin.append("datacriacao", new Date());
                 admin.append("nome", "Administrador");
                 admin.append("login", "admin");
                 admin.append("senha", "21232f297a57a5a743894a0e4a801fc3");
-                admin.append("perfil", perfil.findOne(new BasicDBObject("nome", "Perfil Administrador"), new BasicDBObject("_id", 1)));
+                admin.append("perfil", ob.get("_id"));
                 usuario.insert(admin);
         }
         
